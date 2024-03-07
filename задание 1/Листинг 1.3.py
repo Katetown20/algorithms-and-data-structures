f06ef2d298d9fc624d6acd096063e9c016377168
Katetown20 <https://github.com/Katetown20/test.git>
2024-03-07 18:00:33 +0300

    first commit

---
+++ задание 1/Листинг 1.3.py
@@ -0,0 +1,18 @@
+import  math
+# a = int(input("Введите  угол = "))
+# x = int(input("Введите  х = "))
+# x1 = math.sqrt(math.pi * x) - math.e ** (0.2 * math.sqrt(a))
+# x2 = 2 * math.tan(2 * a)
+# x3 = math.log10(x **2)
+# x4 = 2 * math.tan(2 * a) * (1/(math.cos(x)))
+#
+# y = (x1 + x2 + 1.6 * 10 ** 3 * x3)/x4
+#
+# print(y)
+#
+# вывод
+# Введите  угол = 180
+# Введите  х = 90
+# 414.1856030828704
+
+
